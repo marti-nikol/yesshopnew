3623dc9d2877dd7a55f0848f2d19321b3e60c360
unknown <nikolovmartin@abv.bg>
2020-09-06 16:33:21 +0300

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -20,6 +21,16 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/ochila', require('./routes/api/ochila'));
 app.use('/api/order', require('./routes/api/orders'));
 app.use('/myorder', require('./routes/api/myorder'));
+
+// serve static assets in production
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
